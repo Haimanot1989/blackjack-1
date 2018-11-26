b354301b7e460f7dfb23daacc12b65d30e14000e
Haimanot1989 <haimif@yahoo.com>
2018-11-27 00:29:12 +0100

    Create Card.valueName - Kotlin lesson: IntRange

--- src/test/kotlin/blackjack1/CardTest.kt
+++ src/test/kotlin/blackjack1/CardTest.kt
@@ -13,9 +13,11 @@ class CardTest {
         assertEquals(1, c1.value)
         assertEquals(1, c1.suit)
         assertEquals("Spades", c1.suitName)
+        assertEquals("Ace", c1.valueName)
 
         assertEquals(13, c2.value)
         assertEquals(4, c2.suit)
         assertEquals("Diamonds", c2.suitName)
+        assertEquals("King", c2.valueName)
     }
 }
